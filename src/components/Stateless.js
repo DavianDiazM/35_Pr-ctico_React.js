961a1935d4db94e43ea0b6fd4d7189b72bd99cfc
DavianDiazM <daviandiaz5@gmail.com>
2024-07-20 18:40:23 -0400

    First Commit

---
+++ src/components/Stateless.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const StateLess = () => {   //NO TIENE ESTADOS NI CICLOS DE VIDA
+    return (
+        <h1>Hola Mundazo desde un componente funcional</h1>
+    )
+}
+
+export default StateLess
