69b521fd17366039d15f683d178092a90beda576
DavianDiazM <daviandiaz5@gmail.com>
2024-07-20 18:48:43 -0400

    First Commit Frontend Course

--- src/components/Presentacional.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-//ES UNA TALLA MAS CHEELING
-const Presentacional = () => <h1>Hola desde el presentacional</h1>
-
-export default Presentacional
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"
-import App from "./containers/App.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App/>);
